d3d318923673c151d16d90dc94c4eb977b4c8702
Henry Loveday <henrygad.orji@gmail.com>
2023-11-24 16:57:07 +0100

    version 2.7

--- src/Conponents/Contactform/Contactform.js
+++ src/Conponents/Contactform/Contactform.js
@@ -1,37 +1,76 @@
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+
 import './Contactform.css'
 
-const Contactform = () => {
+
+export const Contactform = () => {
+  const form = useRef();
+  
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [service, setService] = useState('')
+  const [message, setMessage] = useState('')
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+       if(name && email && phone){
+        emailjs.sendForm('service_dyloeel', 'template_svrchpo', form.current, 'ltNawjgy3CqCTwVXe')
+        .then((result) => {
+          alert("message send.", result.text)
+            //console.log(result.text);
+        }, (error) => {
+            alert(error.text)
+            //console.log(error.text);
+        });
+       }else{
+        alert('boxes empty')
+        return;
+       }
+
+
+      setName('')
+      setEmail('')
+      setPhone('')
+      setService('')
+      setMessage('')
+  };
+
   return (
     <div className='contact_us'>
-      <form method='' action='' id='contactform' >
+      <form ref={form} onSubmit={sendEmail} id='contactform'  >
         <div className='grid_wrap'>
           <div className='name'>
-            <label htmlfor='name'>name</label>
-            <input type='text' id='name' placeholder='John mark' />
+            <label htmlFor='name'>name</label>
+            <input type='text' id='name' placeholder='John mark' name="from_name"  value={name} onInput={ e =>  setName(e.target.value)} />
           </div>
           <div className='email' >
-            <label htmlfor='email'>email</label>
-            <input type='email' id='email' placeholder='johnmark@gmail.com' />
+            <label htmlFor='email'>email</label>
+            <input type='email' id='email' placeholder='johnmark@gmail.com' name="from_email" value={email}  onInput={ e => setEmail(e.target.value)}/>
           </div>
           <div className='phone'>
-            <label htmlfor='phone'>phone</label>
-            <input type='phone' id='phone' placeholder='+234 7025672168' />
+            <label htmlFor='phone'>phone</label>
+            <input type='phone' id='phone' placeholder='+234 7025672168' name="from_phone" value={phone}  onInput={ e => setPhone(e.target.value)} />
           </div>
           <div className='service'>
-            <label htmlfor='service'>service</label>
-            <input type='text' id='service' placeholder='Domistic ventilation installation' />
+            <label htmlFor='service'>service</label>
+            <input type='text' id='service' placeholder='Domistic ventilation installation'name="from_service"value={service}  onInput={ e => setService(e.target.value)} />
           </div>
           <div className='message'>
-            <label htmlfor='message'>message us</label>
-            <textarea type='text' id='message' placeholder='Tell us how we can help you...' ></textarea>
+            <label htmlFor='message'>message us</label>
+            <textarea type='text' id='message' placeholder='How can we help you?' name="message" value={message}  onInput={ e => setMessage(e.target.value)} />
           </div>
         </div>
         <div className='form_btn'>
-          <button type='submit'>Submit</button>
+          <input className='contact_button' type="submit" value="Send" />
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
+
+
 
 export default Contactform
